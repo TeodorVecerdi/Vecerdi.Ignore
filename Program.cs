673f011c70aa5492d653df116d5953fcc3af4b7e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-11-20 16:26:33 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System.CommandLine;
+
+internal static class Program {
+    public static async Task<int> Main(string[] args) {
+        RootCommand rootCommand = new() {
+            Name = "ignore",
+            Description = "Generate a .gitignore file based on one or more gitignore.io templates"
+        };
+
+        Command listSubcommand = new("list", "List all available templates");
+        listSubcommand.SetHandler(HandleListCommand);
+        rootCommand.AddCommand(listSubcommand);
+
+        Argument<string[]> ignoresArgument = new(
+            name: "ignores",
+            description: "One or more gitignore.io templates to use"
+        ) {
+            Arity = ArgumentArity.OneOrMore
+        };
+
+        Option<FileInfo> outputOption = new(
+            aliases: new []{"--output", "-o"},
+            description: "Set the output file to write the .gitignore to",
+            getDefaultValue: () => new FileInfo(".gitignore")
+        ) {
+            IsRequired = false,
+            ArgumentHelpName = "file",
+            Arity = ArgumentArity.ExactlyOne,
+        };
+
+        rootCommand.AddArgument(ignoresArgument);
+        rootCommand.AddOption(outputOption);
+        rootCommand.SetHandler(HandleGenerateCommand, ignoresArgument, outputOption);
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    private static async Task HandleListCommand() {
+        Console.WriteLine("Available templates:");
+        await foreach (string template in GetTemplates()) {
+            Console.WriteLine(template);
+        }
+
+        Environment.Exit(0);
+    }
+
+    private static async Task HandleGenerateCommand(string[] templates, FileInfo output) {
+        if (templates is [var joinedTemplates] && joinedTemplates.Contains(',', StringComparison.Ordinal)) {
+            templates = templates[0].Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        }
+
+        Console.WriteLine($"Generating '{output}' using templates: {string.Join(", ", templates)}");
+        string content = await GetContent(templates);
+        await File.WriteAllTextAsync(output.FullName, content);
+    }
+
+    private static async IAsyncEnumerable<string> GetTemplates() {
+        using HttpClient client = new();
+        await using Stream stream = await client.GetStreamAsync("https://www.toptal.com/developers/gitignore/api/list?format=lines");
+        using StreamReader reader = new(stream);
+
+        while (await reader.ReadLineAsync() is { } line) {
+            yield return line;
+        }
+    }
+
+    private static async Task<string> GetContent(string[] templates) {
+        using HttpClient client = new();
+        return await (await client.GetAsync($"https://www.toptal.com/developers/gitignore/api/{string.Join(",", templates)}"))
+                     .Content.ReadAsStringAsync();
+    }
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+# Vecerdi.Ignore
+
+Generate `.gitignore` files for your projects based on one or more templates from [gitignore.io](https://gitignore.io).
+
+## Installation
+
+```bash
+dotnet tool install -g Vecerdi.Ignore
+```
+
+## Usage
+
+```bash
+$ ignore --help
+
+Description:
+  Generate a .gitignore file based on one or more gitignore.io templates
+
+Usage:
+  ignore <ignores>... [command] [options]
+
+Arguments:
+  <ignores>  One or more gitignore.io templates to use
+
+Options:
+  -o, --output <file>  Set the output file to write the .gitignore to [default: .gitignore]
+  --version            Show version information
+  -?, -h, --help       Show help and usage information
+
+
+Commands:
+  list  List all available templates
+```
+
+### Example
+```bash
+$ ignore rider,unity
+$ ignore rider unity --output .gitignore
+```
